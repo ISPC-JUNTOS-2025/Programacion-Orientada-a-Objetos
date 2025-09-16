72c14d7fb57fe519514a5a23d007d7eaf5c257e3
Fernando Agustin Perez <aguss.97@outlook.com>
2025-09-16 10:56:03 -0300

    se suben los enums de Rol, ProgramaLavaRopas y TipoDispositivoDigital

---
+++ POO-SmartHome/__init__.py
@@ -0,0 +1,24 @@
+from .Dispositivo import Dispositivo
+from .DispositivoDigital import DispositivoDigital
+from .Luz import Luz
+from .SensorMovimiento import SensorMovimiento
+from .Camara import Camara
+from .AireAcondicionado import AireAcondicionado
+from .LavaRopas import LavaRopas
+from .Usuario import Usuario
+from .Domicilio import Domicilio, UbicacionCasa
+from .Automatizacion import Automatizacion
+
+__all__ = [
+    'Dispositivo',
+    'DispositivoDigital', 
+    'Luz',
+    'SensorMovimiento',
+    'Camara',
+    'AireAcondicionado',
+    'LavaRopas',
+    'Usuario',
+    'Domicilio',
+    'UbicacionCasa',
+    'Automatizacion'
+]
---
+++ POO-SmartHome/enums/ProgramaLavaRopas.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class ProgramaLavaRopas(Enum):
+    LAVADO_RAPIDO = "lavado_rapido"
+    LAVADO_INTENSIVO = "lavado_intensivo"
+    CENTRIFUGADO = "centrifugado"
+    ENJUAGUE = "enjuague"
+    LAVADO_ECO = "lavado_eco"
+
+
